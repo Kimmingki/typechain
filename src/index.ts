78e6aaa0fbe54d0512e7a1ba73a610c6f34acd4d
Kimmingki <a01050919568@gmail.com>
2025-06-17 13:42:08 +0900

    Blocks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import { init, exit } from "./myPackage";
+import * as crypto from "crypto";
 
-init({
-    debug: true,
-    url: "hello",
-});
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
 
-exit(2);
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+
+    static calculateHash(
+        prevHash: string,
+        height: number,
+        data: string
+    ): string {
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
